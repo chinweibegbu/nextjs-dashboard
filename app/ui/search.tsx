85bbeccf3091e6e878c2595ebef45bc05b6dbc57
chinweibegbu <chinwe.ibegbu@gmail.com>
2024-08-30 14:56:08 +0100

    Add debouncing to search functionality

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,15 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  // Run the code after 300ms once the user has stopped typing
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
@@ -22,7 +24,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     // Replace entire URL with current pathname + "query" search param 
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
