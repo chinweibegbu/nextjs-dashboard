f5bb4a502594e11abc62cdf489a3495632104fee
chinweibegbu <chinwe.ibegbu@gmail.com>
2024-09-02 12:59:06 +0100

    Add accessible form server-side validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,41 +5,69 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
 
-    const rawFormData = {
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    };
+    // const rawFormData = {
+    //     customerId: formData.get('customerId'),
+    //     amount: formData.get('amount'),
+    //     status: formData.get('status'),
+    // };
     // const rawFormData_efficient = Object.fromEntries(formData.entries()); // Alternative approach
 
-    const rawFormDataTypes = Object.fromEntries(
-        Object.entries(rawFormData).map(
-            ([key, value]) => [key, typeof value]
-        )
-    )
+    // const rawFormDataTypes = Object.fromEntries(
+    //     Object.entries(rawFormData).map(
+    //         ([key, value]) => [key, typeof value]
+    //     )
+    // )
 
-    console.log(rawFormData);
-    console.log(rawFormDataTypes);
+    // console.log(rawFormData);
+    // console.log(rawFormDataTypes);
 
-    // Validate data 
-    const { customerId, amount, status } = CreateInvoice.parse({
+    // const { customerId, amount, status } = CreateInvoice.parse({...});
+
+    // Validate data with Zod
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+    
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -62,13 +90,21 @@ export async function createInvoice(formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+    
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try {
@@ -90,8 +126,8 @@ export async function updateInvoice(id: string, formData: FormData) {
 export async function deleteInvoice(id: string) {
     // NOTE: This block is to demonstrate the use of the error.tsx file
     // TODO: Comment out before committing
-    throw new Error('Failed to Delete Invoice');
-    
+    // throw new Error('Failed to Delete Invoice');
+
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         // QUESTION: Why is this non-SQL code in the try...catch() block?
